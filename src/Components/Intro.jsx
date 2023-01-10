fe1e4bbcb8395befd46e2578fb07cd62603d69ac
tatvkamdar79 <kamdartatv79@gmail.com>
2023-01-10 23:53:30 +0530

    fc1

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Intro = () => {
   return (
     <div>
-        <h1 className='font-extrabold text-4xl text-red-700 justify-center text-center'>♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕</h1>
-        <h1 className='font-extrabold text-7xl text-red-700 justify-center text-center'>WISH YOU A VERY HAPPY BIRTHDAY MUMMA</h1>
-        <h1 className='font-extrabold text-4xl text-red-700 justify-center text-center'>♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕</h1>
-        <br /><br /><br />
-        <h1 className='font-extrabold text-7xl text-red-700 justify-center text-center'>WE LOVE YOU MUMMA!!!</h1>
+      <h1 className="font-extrabold text-4xl text-red-700 justify-center text-center">
+        ♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕
+      </h1>
+      <h1 className="font-extrabold text-7xl text-red-700 justify-center text-center">
+        WISH YOU A VERY HAPPY BIRTHDAY MUMMA
+      </h1>
+      <h1 className="font-extrabold text-4xl text-red-700 justify-center text-center">
+        ♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕♥💘💖💗💓💙💚💛💜🧡💝💞💟🖤❤🤍💕
+      </h1>
+      <br />
+      <br />
+      <br />
+      <p>
+        Happy birthday mom You are the sweetest mom in the world You are always
+        there when I need you I know you are always there for me You can take on
+        the entire world for me You are our world With you we are complete,
+        without you we are nothing
+      </p>
+      <h1 className="font-extrabold text-7xl text-red-700 justify-center text-center">
+        WE LOVE YOU MUMMA!!!
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
